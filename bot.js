9dfb9ad0e97e57605a4fed0033370dbf255434d4
kasaasdas <42194246+kasaasdas@users.noreply.github.com>
2018-08-24 01:16:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -64,7 +64,7 @@ hours = hours - 12;
 if (hours == 0) {
 hours = 12;
 }
-      time.setName(`Time :[${hours} : ${minutes} : ${Seconds} ${suffix}]`);
+      time.setName(`Time :[${1} : ${16} : ${50} ${suffix}]`);
  },1000);
   });
   }
