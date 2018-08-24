35a7750e01048f0bb71fde9c2c0cef0bb4ed2da3
kasaasdas <42194246+kasaasdas@users.noreply.github.com>
2018-08-24 18:30:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -119,7 +119,7 @@ client.on('message', msg => {
 
 client.on('message', message => {
               if(!message.channel.guild) return;
-    var prefix = "$";
+    var prefix = "B.";
     if(message.content.startsWith(prefix + 'bc')) {
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
