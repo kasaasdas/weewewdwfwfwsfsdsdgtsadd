6055c463bab07cdd8588e93095d6bda8e5fe04e7
kasaasdas <42194246+kasaasdas@users.noreply.github.com>
2018-08-26 16:04:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -301,4 +301,18 @@ if (message.content.startsWith('*صراحه')) {
 }
 });
 
+client.on('guildMemberAdd', member => {
+    const botCount = member.guild.members.filter(m=>m.user.bot).size
+    const memberCount = [member.guild.memberCount] - [botCount]
+    client.channels.get('ايدي روم 1').setName(`⟫『 ${memberCount} عدد الاعضاء 』⟪`);
+    client.channels.get('ايدي روم 2').setName(`⟫『 ${botCount} عدد البوتات 』⟪`);
+});
+
+client.on('guildMemberRemove', member => {
+    const botCount = member.guild.members.filter(m=>m.user.bot).size
+    const memberCount = [member.guild.memberCount] - [botCount]
+    client.channels.get('ايدي روم 1 (كرر) الي فوق').setName(`⟫『 ${memberCount} عدد الاعضاء 』⟪`);
+    client.channels.get('ايدي روم 2 (كرر) الي فوق').setName(`⟫『 ${botCount} عدد البوتات 』⟪`);
+});
+
 client.login(process.env.BOT_TOKEN);
